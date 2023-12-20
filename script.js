6bcb8d1d76e815a4b76aeed5e8e1514965d896e4
Hassan <hassanakbar2012@hotmail.com>
2023-12-20 14:26:44 +0000

    added the event listeners to open, close buttons and also the overlay (area outside the modal)

--- script.js
+++ script.js
@@ -5,11 +5,28 @@
     * close-modal-btn
     * BONUS: overlay
 */
-
 // TODO: 3. Create a click event listener for the open-modal-btn that adds the class "open" to the modal
 // BONUS: Also add the class "open" to the overlay
-
 // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
 // BONUS: Also remove the class "open" from the overlay
-
 // BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
+
+const modal = document.querySelector("#modal")
+const closeButton = document.querySelector("#open-modal-btn")
+const openButton = document.querySelector("#close-modal-btn")
+const overlay = document.querySelector("#overlay")
+
+closeButton.addEventListener("click", () => {
+  modal.classList.add("open")
+  overlay.classList.add("open")
+})
+
+openButton.addEventListener("click", () => {
+  modal.classList.remove("open")
+  overlay.classList.remove("open")
+})
+
+overlay.addEventListener("click", () => {
+  modal.classList.remove("open")
+  overlay.classList.remove("open")
+})
